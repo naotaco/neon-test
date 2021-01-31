3db73158051dc5c7556c129947ca75f812fe6e6a
Nao Taco <naotaco@gmail.com>
2021-02-01 00:24:41 +0900

    update

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CXX=clang++
 TARGET=neon-test
 OBJS:=$(SRCS:.cpp=.o)
 
-CXXFLAGS:= -Wall -mfloat-abi=hard -mfpu=neon -march=armv8-a
+CXXFLAGS:= -Wall -mfloat-abi=hard -mfpu=neon -march=armv8-a --debug -O2
 
 all: $(OBJS)
 	$(CXX) -o $(TARGET) $<
--- neon01.cpp
+++ neon01.cpp
@@ -3,5 +3,17 @@
 
 int main(int argc, char** argv){
   std::cout << "Hello neon" << std::endl;
+  unsigned char src0[] = {  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15};
+  unsigned char src1[] = {100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115};
+  unsigned char dst [] = {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0};
+  uint8x16_t s0 = vld1q_u8(src0);
+  uint8x16_t s1 = vld1q_u8(src1);
+  uint8x16_t d  = vaddq_u8(s0, s1); // ここで足し算
+  vst1q_u8(dst, d);
+  for( auto i = 0;i < 16;i++)
+    {
+      std::cout << i << '\t' << (int)src0[i] << '\t' << (int)src1[i] << '\t' << (int)dst[i] << std::endl;
+    }
+  
   return 0;
 }
